6597ac29aba09e00c758d946145cc2564be39a4e
Berzenin Filipp <filipp.berzenin@gmail.com>
2016-08-30 16:47:57 +0300

    SimplyLinkedList

---
+++ src/Lesson_4/Frame_2/OurList/Note.java
@@ -0,0 +1,29 @@
+package Lesson_4.Frame_2.OurList;
+
+import java.util.Objects;
+
+public class Note {
+
+    private Objects o;
+    private Note note;
+
+    Note () {
+
+    }
+
+    public Objects getO() {
+        return o;
+    }
+
+    public void setO(Objects o) {
+        this.o = o;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+}
