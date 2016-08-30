abd0be7f84a9f7af2bb82c44eed0bdb3bfd19a94
Berzenin Filipp <filipp.berzenin@gmail.com>
2016-08-30 16:47:20 +0300

    SimplyLinkedList

---
+++ src/Lesson_4/Frame_2/OurList/SimplyLinkedList.java
@@ -0,0 +1,25 @@
+package Lesson_4.Frame_2.OurList;
+
+import java.util.Objects;
+
+public class SimplyLinkedList {
+
+    Note root;
+    int size;
+
+    public void addFirstObject (Object o) {
+
+    }
+
+    public void addLastObject (Object o) {
+
+    }
+
+    public void addAfterObject (Object f, Object s) {
+
+    }
+
+    public int size () {
+        return size;
+    }
+}
