72230ac46a689b45c125557c72559a9c6e63bf8c
Berzenin Filipp <filipp.berzenin@gmail.com>
2016-09-30 15:40:03 +0300

    jast do it

--- src/Lesson_5/Frame_1/Window.java
+++ src/Lesson_5/Frame_1/Window.java
@@ -3,13 +3,38 @@ package Lesson_5.Frame_1;
 import javax.swing.*;
 import java.awt.*;
 
-public class Window {
+public class Window extends JPanel {
 
-    public static void main (String[] args) {
-        JFrame window = new JFrame();
-        window.setVisible(true);
-        window.setLocation(300,100);
-        window.setMinimumSize(new Dimension(800,600));
+    public Window()  {
+        JFrame f = new JFrame("My graphics 2D");
+        f.setMinimumSize(new Dimension(800, 600));
+        f.setLocation(300, 100);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(this);
+        f.pack();
+        f.setVisible(true);
     }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(new Color(203,30,0));
+        g.fillRect(0, 0, 800, 600);
+
+        g.setColor(new Color(86, 142, 204));
+        g.fillRect(400, 100, 50, 50);
+
+        g.setColor(new Color(34, 174, 93));
+        g.draw3DRect(400, 200, 49, 49, true);
+
+        g.setColor(new Color(255,255,255));
+        g.setFont(new Font("BOLD",Font.PLAIN,32));
+        g.drawString("Jast do it", 20, 50);
+
+    }
+    public static void main(String[] args)  {
+        new Window();
+
+    }
+
 
 }
