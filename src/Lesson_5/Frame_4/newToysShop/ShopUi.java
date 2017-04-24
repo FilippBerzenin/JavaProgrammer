c414d6cc682a035abfc67bb20b83dc071672675a
Berzenin Filipp <filipp.berzenin@gmail.com>
2017-04-24 14:30:57 +0300

    panel.setLayout(new GridBagLayout());

--- src/Lesson_5/Frame_4/newToysShop/ShopUi.java
+++ src/Lesson_5/Frame_4/newToysShop/ShopUi.java
@@ -27,38 +27,44 @@ public class ShopUi extends JPanel {
 
     private JPanel createSellimgPanel() {
         JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
 
         JLabel name1 = new JLabel("Your name");
         JTextField tf = new JTextField();
-        panel.add(name1);
-        panel.add(tf);
+        tf.setColumns(5);
+        panel.add(name1, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(0,0,0,0),0,0));
+        panel.add(tf,    new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0),0,0));
 
         JLabel lproduct = new JLabel("Cars:");
 
         ButtonGroup bg = new ButtonGroup();
         JPanel pp = new JPanel();
+        pp.setLayout(new GridLayout(stock.size(), 0));
+        pp.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+
         for (int i = 0; i<stock.size(); i++) {
             JRadioButton pc = new JRadioButton(stock.get(i)[0].getName()+" "+stock.get(i)[0].getPrice());
             bg.add(pc);
             pp.add(pc);
         }
 
-        panel.add(lproduct);
-        panel.add(pp);
+        panel.add(lproduct, new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(3,0,0,0),0,0));
+        panel.add(pp,       new GridBagConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(3,0,0,0),0,0));
 
             JLabel count = new JLabel("Count");
             NumberFormat nf = NumberFormat.getNumberInstance();
             JFormattedTextField tfCount = new JFormattedTextField(nf);
+            tfCount.setColumns(2);
             tfCount.setValue(0);
-            panel.add(count);
-            panel.add(tfCount);
+            panel.add(count, new GridBagConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(3,0,0,0),0,0));
+            panel.add(tfCount, new GridBagConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(3,0,0,0),0,0));
 //
 //            JTextField tfAmount = new JTextField();
 //            panel.add(tfAmount);
 
 
         JButton buttonBuy = new JButton("Buy");
-        panel.add(buttonBuy);
+        panel.add(buttonBuy, new GridBagConstraints(1, 3, 1, 1, 0, 0, GridBagConstraints.LINE_START, 0, new Insets(3,0,0,0),0,0));
 
 
         return panel;
